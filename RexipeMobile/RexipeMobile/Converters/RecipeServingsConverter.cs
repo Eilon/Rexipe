8bdf31487f20d07bf6c06540a55f6785465f0242
Eilon Lipton <Eilon@users.noreply.github.com>
2019-06-28 13:55:37 -0700

    Add converter for recipe servings to show serving range

---
+++ RexipeMobile/RexipeMobile/Converters/RecipeServingsConverter.cs
@@ -0,0 +1,33 @@
+﻿using RexipeModels;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace RexipeMobile.Converters
+{
+    public class RecipeServingsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var recipe = value as Recipe;
+            if (recipe == null)
+            {
+                return null;
+            }
+
+            if (recipe.ServingsMin == recipe.ServingsMax)
+            {
+                return recipe.ServingsMin.ToString(culture);
+            }
+            else
+            {
+                return string.Format(culture, "{0} - {1}", recipe.ServingsMin, recipe.ServingsMax);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
