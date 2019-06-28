19758c6d2ade42ac10f151d42dc4c7ba846514ae
Eilon Lipton <Eilon@users.noreply.github.com>
2019-06-28 16:46:01 -0700

    Show more details on recipe info page

---
+++ RexipeMobile/RexipeMobile/Converters/TimeSpanConverter.cs
@@ -0,0 +1,40 @@
+﻿using RexipeModels;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace RexipeMobile.Converters
+{
+    public class TimeSpanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is TimeSpan))
+            {
+                return null;
+            }
+            var timeSpan = (TimeSpan)value;
+
+            if (timeSpan.TotalMinutes > 60)
+            {
+                if (timeSpan.Minutes == 0)
+                {
+                    return string.Format(culture, "{0}hr", timeSpan.Hours);
+                }
+                else
+                {
+                    return string.Format(culture, "{0}hr {1}min", timeSpan.Hours, timeSpan.Minutes);
+                }
+            }
+            else
+            {
+                return string.Format(culture, "{0}min", timeSpan.Minutes);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
