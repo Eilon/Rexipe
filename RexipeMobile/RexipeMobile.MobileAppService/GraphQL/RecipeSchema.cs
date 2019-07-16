c779ad24b30d1cc86b4af2364716ba1645009ef6
Eilon Lipton <Eilon@users.noreply.github.com>
2019-07-16 16:19:11 -0700

    Initial GraphQL query support
    - Added GraphQL package refs
    - Added basic GraphQL schema and types (incomplete)
    - Added GraphQL endpoints

---
+++ RexipeMobile/RexipeMobile.MobileAppService/GraphQL/RecipeSchema.cs
@@ -0,0 +1,85 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using RexipeMobile.Models;
+using RexipeModels;
+
+namespace RexipeMobile.MobileAppService.GraphQL
+{
+    public class RecipeSchema : Schema
+    {
+        public RecipeSchema(IDependencyResolver resolver) : base(resolver)
+        {
+            Query = resolver.Resolve<RecipeQuery>();
+            //Mutation = new RecipeMutation(recipe);
+            //Subscription = new RecipeSubscriptions(recipe);
+        }
+    }
+
+    public class RecipeQuery : ObjectGraphType
+    {
+        public RecipeQuery(RecipeRepository recipeRepo)
+        {
+            Name = "RecipeDb";
+            Description = "A database of recipes.";
+
+            Field<ListGraphType<RecipeType>>("recipes", resolve: context => recipeRepo.GetAll(), description: "Recipes in the database.");
+        }
+    }
+
+    public class RecipeType : ObjectGraphType<Recipe>
+    {
+        public RecipeType(RecipeRepository recipeRepo)
+        {
+            Field(x => x.Id, type: typeof(IdGraphType));
+
+            Field(x => x.Title).Description("The title of the recipe.");
+            Field(x => x.ServingsMin).Description("The minimum servings this recipe will produce.");
+            Field(x => x.ServingsMax).Description("The maximum servings this recipe will produce.");
+
+            Field<ListGraphType<IngredientQuantityType>>("ingredients", resolve: context => recipeRepo.GetRecipeIngredients(context.Source.Id));
+        }
+    }
+
+    public class IngredientQuantityType : ObjectGraphType<IngredientQuantity>
+    {
+        public IngredientQuantityType()
+        {
+            Field(x => x.Id, type: typeof(IdGraphType));
+
+            Field(x => x.Ingredient, type: typeof(IngredientType)).Description("The ingredient.");
+            Field(x => x.Quantity, type: typeof(ItemQuantityType)).Description("The quantity of the ingredient.");
+        }
+    }
+
+    public class IngredientType : ObjectGraphType<Ingredient>
+    {
+        public IngredientType()
+        {
+            Field(x => x.Id, type: typeof(IdGraphType));
+
+            Field(x => x.Name).Description("The name of the ingredient.");
+        }
+    }
+
+    public class ItemQuantityType : ObjectGraphType<ItemQuantity>
+    {
+        public ItemQuantityType()
+        {
+            Field(x => x.Id, type: typeof(IdGraphType));
+
+            Field(x => x.Numerator).Description("The numerator of the ingredient quantity.");
+            Field(x => x.Denominator).Description("The denominator of the ingredient quantity.");
+            Field(x => x.OtherUnit, nullable: true).Description("The custom unit of the ingredient quantity.");
+            Field<IngredientUnitType>("Unit", description: "The unit of the ingredient quantity.");
+        }
+    }
+
+    public class IngredientUnitType : EnumerationGraphType<IngredientUnit>
+    {
+        public IngredientUnitType()
+        {
+            //Name = "";
+            //Desc
+        }
+    }
+}
--- RexipeMobile/RexipeMobile.MobileAppService/Startup.cs
+++ RexipeMobile/RexipeMobile.MobileAppService/Startup.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using GraphQL;
+using GraphQL.Server;
+using GraphQL.Server.Ui.GraphiQL;
+using GraphQL.Server.Ui.Playground;
+using GraphQL.Server.Ui.Voyager;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RexipeMobile.MobileAppService.Data;
+using RexipeMobile.MobileAppService.GraphQL;
 using RexipeMobile.Models;
 using Swashbuckle.AspNetCore.Swagger;
 
@@ -12,12 +18,14 @@ namespace RexipeMobile.MobileAppService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
         {
             Configuration = configuration;
+            HostingEnvironment = hostingEnvironment;
         }
 
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment HostingEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -36,11 +44,26 @@ namespace RexipeMobile.MobileAppService
             {
                 c.SwaggerDoc("v1", new Info { Title = "Rexipe API", Version = "v1" });
             });
+
+
+            // For GraphQL
+            services.AddScoped<IDependencyResolver>(s => new FuncDependencyResolver(s.GetRequiredService));
+            services.AddScoped<RecipeSchema>();
+
+            // Add GraphQL services and configure options
+            services.AddGraphQL(options =>
+            {
+                options.EnableMetrics = true;
+                options.ExposeExceptions = HostingEnvironment.IsDevelopment();
+            })
+                .AddGraphTypes(ServiceLifetime.Scoped);
+            //.AddWebSockets() // Add required services for web socket support
+            //.AddDataLoader(); // Add required services for DataLoader support
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            if (HostingEnvironment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -53,6 +76,18 @@ namespace RexipeMobile.MobileAppService
             }
 
             app.UseMvc();
+
+            // use HTTP middleware for ChatSchema at path /graphql
+            app.UseGraphQL<RecipeSchema>("/graphql");
+
+            // use graphiQL middleware at default url /graphiql
+            app.UseGraphiQLServer(new GraphiQLOptions());
+
+            // use graphql-playground middleware at default url /ui/playground
+            app.UseGraphQLPlayground(new GraphQLPlaygroundOptions());
+
+            // use voyager middleware at default url /ui/voyager
+            app.UseGraphQLVoyager(new GraphQLVoyagerOptions());
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>
