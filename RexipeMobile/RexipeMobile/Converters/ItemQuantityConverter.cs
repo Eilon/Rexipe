7b515cadb1c5c37eb44500a6171a83114dd00730
Eilon Lipton <Eilon@users.noreply.github.com>
2019-07-17 11:25:12 -0700

    Improve recipe detail page UI
    Show item quantities
    Remove duplicate info
    Make fonts smaller

---
+++ RexipeMobile/RexipeMobile/Converters/ItemQuantityConverter.cs
@@ -0,0 +1,25 @@
+﻿using RexipeModels;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace RexipeMobile.Converters
+{
+    public class ItemQuantityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is ItemQuantity itemQuantity))
+            {
+                return null;
+            }
+
+            return itemQuantity.GetItemQuantity();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
